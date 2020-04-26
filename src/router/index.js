7f571ba7dbf7095e53b2012349c48cf283dd1f43
王霸胆 <574478423@qq.com>
2020-04-26 23:27:03 +0800

    搭建完成

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+// 导入 Vue
+import Vue from 'vue'
+// 导入 router
+import VueRouter from 'vue-router'
+
+// 使用vue-router
+Vue.use(VueRouter)
+
+var router = new VueRouter({
+  // 添加路由规则
+  routes: [
+
+  ]
+})
+
+// 将 vue-router 暴露出去
+export default router
